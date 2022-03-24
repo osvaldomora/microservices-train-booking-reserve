503d81a5f91ff608df7f90255a6e43fb1f89ced2
osvaldo <osvaelectro@gmail.com>
2022-03-24 12:31:07 -0600

    update:project was created

---
+++ train-ticket-booking-eureka-server/src/test/java/train/ticket/booking/app/eureka/server/TrainTicketBookingEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package train.ticket.booking.app.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketBookingEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ train-ticket-booking-user/src/test/java/train/ticket/booking/app/user/TrainTicketBookingUserApplicationTests.java
@@ -0,0 +1,13 @@
+package train.ticket.booking.app.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketBookingUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
