15e439a782e94317e24629c92df451eeca6bd601
Jose Esquinca <esquinca_jose@hotmail.com>
2022-03-31 16:50:10 -0500

    fixing passengers sonar

--- train-ticket-booking-passenger-service/src/test/java/train/ticket/booking/app/passenger/TrainTicketBookingUserApplicationTests.java
+++ train-ticket-booking-passenger-service/src/test/java/train/ticket/booking/app/passenger/TrainTicketBookingUserApplicationTests.java
@@ -1,4 +1,4 @@
-package train.ticket.booking.app.train;
+package train.ticket.booking.app.passenger;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
